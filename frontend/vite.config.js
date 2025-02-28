3918b46684d221e148dfb1f44a8f6a9e56f3a4e3
monireach2480 <lenmonireach123@gmail.com>
2025-02-28 22:31:38 +0700

    dfdf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/https://full-stack-notes-application-4.onrender.com/'
+
 })
